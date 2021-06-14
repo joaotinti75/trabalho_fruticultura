24e1b4841447ce4a2affb459b712a9252eaba997
Joao Tinti <joaotinti75@gmail.com>
2021-06-14 12:00:02 -0300

    implementacao do quiz

---
+++ quiz-model.js
@@ -0,0 +1,42 @@
+const respostasCorretas = {
+    r1:'São Paulo',
+    r2:'Rutaceae',
+    r3:'8 a 10 meses', 
+    r4:'Pera',
+    r5:'Greening',
+    r6:'Vitamina C',
+    r7:'amarelecimento das folhas mais velhas, folhas ficam pálidas, tamanho reduzido e caem precocemente',
+    r8:'amarelecimento ao longo das margens nas folhas maduras',
+    r9:'Manchas grandes, amarelo-brilhantes, circulares ou elípticas entre as nervuras',
+    r10:'amarelecimento mosqueado na folha nas folhas jovens'
+};
+
+const perguntas = [
+    'Qual é o maior estado produtor de citrus no Brasil?',
+    'Qual a família dos citros?',
+    'Quanto tempo dura a safra dos citros?',
+    'Qual a variedade mais plantada?',
+    'Qual a principal doença dos citros?',
+    'Qual a principal vitamina proveniente dos citros?',
+    'Quais são os sintomas de deficiência de nitrogenio?',
+    'Quais são os sintomas de deficiência de Calcio?',
+    'Quais são os sintomas de deficiência de molibdênio?',
+    'Quais são os sintomas de deficiência de manganês?'
+]
+
+alternativas = [
+    {a:'São Paulo', b:'Paraná', c:'Pernambuco', d:'Rio de Janeiro'},
+    {a:'Rutaceae', b:'Fabaceae', c:'Gramínea', d:'Malvaceae'},
+    {a:'3 meses', b:'4 a 6 meses', c:'8 a 10 meses', d:'8 meses'},
+    {a:'Rubi', b:'Pera', c:'Lima', d:'Natal'},
+    {a:'Greening', b:'Antracnose', c:'Cancro-cítrico', d:'Gomose'},
+    {a:'Vitamina C', b:'Vitamina A', c:'Vitamina B', d:'Vitamina D'},
+
+    {a:'amarelecimento das folhas mais velhas, folhas ficam pálidas, tamanho reduzido e caem precocemente', b:'amarelecimento mosqueado na folha nas folhas jovens', c:'amarelecimento ao longo das margens nas folhas maduras', d:'Manchas grandes, amarelo-brilhantes, circulares ou elípticas entre as nervuras'},
+
+    {a:'amarelecimento das folhas mais velhas, folhas ficam pálidas, tamanho reduzido e caem precocemente', b:'amarelecimento mosqueado na folha nas folhas jovens', c:'amarelecimento ao longo das margens nas folhas maduras', d:'Manchas grandes, amarelo-brilhantes, circulares ou elípticas entre as nervuras'},
+
+    {a:'amarelecimento das folhas mais velhas, folhas ficam pálidas, tamanho reduzido e caem precocemente', b:'amarelecimento mosqueado na folha nas folhas jovens', c:'amarelecimento ao longo das margens nas folhas maduras', d:'Manchas grandes, amarelo-brilhantes, circulares ou elípticas entre as nervuras'},
+
+    {a:'amarelecimento das folhas mais velhas, folhas ficam pálidas, tamanho reduzido e caem precocemente', b:'amarelecimento mosqueado na folha nas folhas jovens', c:'amarelecimento ao longo das margens nas folhas maduras', d:'Manchas grandes, amarelo-brilhantes, circulares ou elípticas entre as nervuras'}
+]
